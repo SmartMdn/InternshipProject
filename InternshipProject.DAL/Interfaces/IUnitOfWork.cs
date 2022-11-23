c9507e158aef3f5dc787be26a4bbc15867e3bb23
IlyaMatyunin <IlyaMatyunin@KOMPUTER>
2022-11-23 16:47:10 +0300

    Add Interfaces for realize "Unit To Work" and "Repository" patterns

---
+++ InternshipProject.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using InternshipProject.DAL.Entities;
+
+namespace InternshipProject.DAL.Interfaces;
+
+public interface IUnitOfWork : IDisposable
+{
+    IRepository<Event> Events { get;}
+    IRepository<Hall> Halls { get;}
+    IRepository<Place> Places { get;}
+    IRepository<Section> Sections { get;}
+    IRepository<Stadium> Stadiums { get;}
+    IRepository<Ticket> Tickets { get;}
+    void Save();
+}
