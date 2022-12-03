1f76c4020a489e02bb6a33e031272276e99a92a4
SmartMdn <i.matyuninn@gmail.com>
2022-12-04 01:40:03 +0300

    Create CRUD Controller for Controllers

---
+++ InternshipProject.WEB/Interfaces/CrudController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using InternshipProject.BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InternshipProject.WEB.Interfaces;
+
+public abstract class CrudController<T, TK> : ControllerBase where TK : class
+{
+    protected readonly IMapper MapperInput = new MapperConfiguration(cfg => cfg.CreateMap<T, TK>()).CreateMapper();
+    protected readonly IMapper MapperOutput = new MapperConfiguration(cfg => cfg.CreateMap<TK, T>()).CreateMapper();
+    protected readonly ICRUDService<TK> Service;
+    protected TK ResultItem;
+
+    protected CrudController(ICRUDService<TK> service)
+    {
+        Service = service;
+    }
+
+    public abstract T Get(int id);
+    public abstract string Put(T item);
+    public abstract string Post(T item);
+    public abstract string Delete(int id);
+}
